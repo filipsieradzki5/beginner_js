d14c3e23ae550131ac80c016edc422da8749404e
Filip Sieradzki <sieradzki.filiP@gmail.comp>
2020-08-24 22:58:26 +0200

    guess whos back

--- exercises/33 - Etch-a-Sketch/etch-a-sketch-FINISHED.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch-FINISHED.js
@@ -2,7 +2,7 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
-const MOVE_AMOUNT = 50;
+const MOVE_AMOUNT = 10;
 // Setup our canvas for drawing
 // make a variable called height and width from the same properties on our canvas.
 const { width, height } = canvas;
