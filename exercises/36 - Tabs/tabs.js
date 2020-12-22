8d6995a954151a612550089b86e1c5f31f3475e9
Filip Sieradzki <sieradzki.filiP@gmail.comp>
2020-12-22 01:53:39 +0100

    im gonna need ur id

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,24 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+// hide all tab panels
+tabPanels.forEach(p => {
+    p.hidden = true;
+});
+// mark all tabs as unselected
+tabButtons.forEach(tab => {
+    tab.setAttribute('aria-selected',false);
+});
+// mark the clicked tabs as selected
+event.currentTarget.setAttribute('aria-selected', true);
+// find the associated tabPanel and show it
+const { id } = event.currentTarget;
+console.log(id);
+
+const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+tabPanel.hidden = false;
+}
+tabButtons.forEach(button => button.addEventListener
+    ('click', handleTabClick));
